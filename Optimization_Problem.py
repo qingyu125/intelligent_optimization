16feef8cbfe6581e04240d6dd9c6a50caddaaab8
Han Wang <169051861+qingyu125@users.noreply.github.com>
2025-06-04 18:53:50 +0800

    Update Optimization_Problem.py

--- Optimization_Problem.py
+++ Optimization_Problem.py
@@ -40,7 +40,7 @@ class RastriginProblem(OptimizationProblem):
 
 # 离散优化问题 - 0-1背包问题
 class KnapsackProblem(OptimizationProblem):
-    def __init__(self, n_items: int = 20, max_weight: int = 500):
+    def __init__(self, n_items: int = 20, max_weight: int = 50):
         super().__init__(n_items, 0, 1, "Knapsack")
         self.max_weight = max_weight
         # 随机生成物品价值和重量
